4dc95216a693c254ad5a3e108c673fc251e06f92
joonlenie <phanalensarik2017@gmail.com>
2022-03-04 19:10:12 +0700

    ...

---
+++ myfile.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.your-class').slick({
+    setting-name: setting-value
+  });
+});
+
+(function($) {
+    $(document).ready(function() {
+      $('.fade').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
